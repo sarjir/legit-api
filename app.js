0d00a62a22b295f23924f6ef9bef22328f5cdcad
Sara Jirholm <sara.jirholm@hotmail.com>
2019-08-01 12:49:26 +0200

    Add connection to mongo client. Retrive articles from collection

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var express = require("express");
 var graphqlHTTP = require("express-graphql");
+const MongoClient = require("mongodb").MongoClient;
 var { buildSchema } = require("graphql");
+const assert = require("assert");
+
+const dbUrl = "mongodb://database:27017";
+const dbName = "legit";
+const client = new MongoClient(dbUrl);
+let db;
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
@@ -12,7 +19,17 @@ var schema = buildSchema(`
 // The root provides a resolver function for each API endpoint
 var root = {
   hello: () => {
-    return "Hello worldzzzzz!";
+    let articles;
+
+    return db
+      .collection("articles")
+      .find()
+      .toArray()
+      .then((result, error) => {
+        console.log("result", result);
+
+        return JSON.stringify(result);
+      });
   }
 };
 
@@ -27,3 +44,11 @@ app.use(
 );
 app.listen(4000);
 console.log("Running a GraphQL API server at localhost:4000/graphql");
+
+client.connect(function(err) {
+  console.log("connecting to database!");
+  assert.equal(null, err);
+  console.log("Database connected successfully");
+
+  db = client.db(dbName);
+});
